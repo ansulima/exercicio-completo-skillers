d60e66c2c45afeb5872706d2d6ffdf4926fab72a
Anderson Freieitas <freitaslimaanderson@gmail.com>
2024-05-08 00:19:42 -0300

    Resolving Exercise 02

---
+++ LISTA-EXERCICIOS/exercicio_02_nome_endereco_telefone.rb
@@ -0,0 +1,8 @@
+puts "Digite seu nome: "
+nome = gets.chomp.to_s
+puts "Digite seu endereço: "
+endereco = gets.chomp.to_s
+puts "Digite seu telefone: "
+telefone = gets.chomp.to_i
+
+puts "Seu nome é #{nome}, seu endereço é #{endereco} e seu telefone é #{telefone}"
